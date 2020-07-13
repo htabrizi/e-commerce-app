6b41b565ef7cd471552a026491c25a0ff0de7ad1
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-13 13:04:45 +0430

    4.1.1 Conditional

--- Prepared e-commerce/ConsoleApp1/ConsoleApp1/Program.cs
+++ Prepared e-commerce/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("enter Number");
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+            if (number >= 1 && number <= 10)
+            { Console.WriteLine("valid"); }
+            else
+            {
+                Console.WriteLine("invalid"); }
+           
         }
     }
 }
+ 
