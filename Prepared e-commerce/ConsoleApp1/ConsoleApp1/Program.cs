c0ccfc9fc27d4608df1591c8d54eb30195a5f991
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-13 15:27:42 +0430

    4.1.4 Conditional

--- Prepared e-commerce/ConsoleApp1/ConsoleApp1/Program.cs
+++ Prepared e-commerce/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,17 +8,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Image width: ");
-            var width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is the speed limit? ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Image height: ");
-            var height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is the speed of this car? ");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
 
-            var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
-            Console.WriteLine("Image orientation is " + orientation);
+            if (carSpeed < speedLimit)
+                Console.WriteLine("Ok");
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (carSpeed - speedLimit) / kmPerDemeritPoint;
+                if (demeritPoints > 12)
+                    Console.WriteLine("License Suspended");
+                else
+                    Console.WriteLine("Demerit points: " + demeritPoints);
+            }
+
+
         }
-
-
     }
-
 }
