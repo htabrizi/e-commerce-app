166870f424ed493e0759eeadf61416f668a7200b
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-15 00:01:38 +0430

    5.2.2 Demo

--- Prepared e-commerce/ConsoleApp1/ConsoleApp1/Program.cs
+++ Prepared e-commerce/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,16 +9,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var count = 0;
-            for(var i=1; i<=100; i++)
-            {
-                if (i % 3 == 0)
-                {
-                    count++;
 
+            var sum = 0;
+
+            while (true)
+            {
+                Console.Write("write 2 number");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "ok")
+                {
+                    break;
                 }
+                sum += Convert.ToInt32(input);
             }
-            Console.WriteLine("There are {0} numbers divisible by 3 between 1 and 100.", count);
+            Console.WriteLine(sum);
         }
     }
 }
