38062e8e5c8b7c61e763050f09ab65d94ec42cbc
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-14 10:17:04 +0430

    2.15 Test reading the data from the Database in the API

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -1,21 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
 
-    public class ProductsController:ControllerBase  
+    public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            return "this will be a list of products";
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products=await _context.Products.ToListAsync();
+            return Ok(products);
 
         }
         [HttpGet("{id}")]
-        public string GetProduct(int id){
-            return "single product";
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
         }
     }
 }
