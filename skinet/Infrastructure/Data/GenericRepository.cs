6a85fa3c1c08b8ae49b66361ed078cf88ddd05cd
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-20 20:32:23 +0430

    38 Implementing the repository with specification methods

--- skinet/Infrastructure/Data/GenericRepository.cs
+++ skinet/Infrastructure/Data/GenericRepository.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -19,9 +21,24 @@ namespace Infrastructure.Data
             return await _context.Set<T>().FindAsync(id); 
         }
 
+    
+
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _context.Set<T>().ToListAsync();
         }
+
+    public async Task<T> GetEntityWhitSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+ {
+     return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(),spec);
+ }       
     }
 }
