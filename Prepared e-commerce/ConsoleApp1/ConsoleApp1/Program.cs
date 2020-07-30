ab8724b0da9c625ef81d38538ae08126da74ea27
Hamid-Tabrizi <h.tabrizi@orbs.ir>
2020-07-30 13:18:50 +0430

    6-5-2 LIST

--- Prepared e-commerce/ConsoleApp1/ConsoleApp1/Program.cs
+++ Prepared e-commerce/ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,17 +15,28 @@ namespace ConsoleApp1
          numbers.Add(1);
          numbers.AddRange(new int[3]{3,3,3});
          foreach (var number in numbers)
-             Console.WriteLine(number);
+         
+
+         Console.WriteLine(number);
 
          Console.WriteLine();
          Console.WriteLine(numbers.IndexOf(1));
          Console.WriteLine(numbers.LastIndexOf(1));
          Console.WriteLine(numbers.Count);
-         numbers.Remove(1);
+
+         for (var index = 0; index < numbers.Count; index++)
+         {
+             var number = numbers[index];
+             if (number == 1)
+             {
+                 numbers.Remove(number);
+             }
+         }
+
          foreach (var number in numbers)
-             Console.WriteLine(number);
-
-
+            Console.WriteLine(number);
+numbers.Clear();
+Console.WriteLine(numbers.Count);
         }
 
     }
